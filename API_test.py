c51adae745f176f6a25cb8a05668a7f3a30eb71c
Radek <43780738+RadekHM@users.noreply.github.com>
2020-05-08 21:08:15 +0300

    API test

---
+++ API_test.py
@@ -0,0 +1,24 @@
+"Test for Api"
+import unittest
+import requests
+
+class TestMathFunction(unittest.TestCase):
+    "Class to test Api"
+    def test_api(self):
+        "Tests for the api"
+        url = "https://corona-virus-stats.herokuapp.com/api/v1/" \
+              "cases/countries-search?search=Ukraine"
+        test = requests.get(url)
+        status_code = test.status_code
+        expected = 200
+        data = test.json()
+
+        status = data["status"]
+        status_message = "success"
+
+        self.assertEqual(status_code, expected)
+        self.assertEqual(status, status_message)
+
+
+if __name__ == '_main_':
+    unittest.main()
